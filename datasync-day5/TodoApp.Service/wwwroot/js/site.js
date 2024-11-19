79cadb92e93be1a41c57726dffc78f9062957016
Adrian Hall <adhal@microsoft.com>
2024-11-18 16:17:30 -0800

    Added datasync service interactions.

--- datasync-day5/TodoApp.Service/wwwroot/js/site.js
+++ datasync-day5/TodoApp.Service/wwwroot/js/site.js
@@ -5,6 +5,65 @@
         return a === b ? options.fn(this) : options.inverse(this);
     });
 
+    const DatasyncService = {
+        fetchTodoItems: async () => {
+            console.debug('DatasyncService.fetchTodoItems');
+            const response = await fetch('/tables/todoitems');
+            console.debug('DatasyncService.fetchTodoItems: response = ', response);
+            if (response.ok) {
+                const data = await response.json();
+                console.debug('DatasyncService.fetchTodoItems: data = ', data);
+                return data.items;
+            }
+            throw new Error(`Failed to fetch todo items: status=${response.status}`);
+        },
+        createTodoItem: async (title) => {
+            console.debug('DatasyncService.createTodoItem: title = ', title);
+            const response = await fetch('/tables/todoitems', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({ title: title })
+            });
+            console.debug('DatasyncService.createTodoItem: response = ', response);
+            if (response.ok) {
+                const data = await response.json();
+                console.debug('DatasyncService.createTodoItem: data = ', data);
+                return data;
+            }
+            throw new Error(`Failed to create todo item: status=${response.status}`);
+        },
+        editTodoItem: async (todoItem) => {
+            console.debug('DatasyncService.editTodoItem: todoItem = ', todoItem);
+            const response = await fetch(`/tables/todoitems/${todoItem.id}`, {
+                method: 'PUT',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify(todoItem)
+            });
+            console.debug('DatasyncService.editTodoItem: response = ', response);
+            if (response.ok) {
+                const data = await response.json();
+                console.debug('DatasyncService.editTodoItem: data = ', data);
+                return data;
+            }
+            throw new Error(`Failed to edit todo item: status=${response.status}`);
+        },
+        removeTodoItem: async (todoItem) => {
+            console.debug('DatasyncService.removeTodoItem: todoItem = ', todoItem);
+            const response = await fetch(`/tables/todoitems/${todoItem.id}`, {
+                method: 'DELETE'
+            });
+            console.debug('DatasyncService.removeTodoitem: response = ', response);
+            if (response.status === 204) {
+                return;
+            }
+            throw new Error(`Failed to remove todo item: status=${response.status}`);
+        },
+    };
+
     let todoItems = [];
     let listFilter = 'all';
 
@@ -50,64 +109,60 @@
         }));
     };
 
-    const createTodoItem = (title) => {
+    const createTodoItem = async (title) => {
         console.debug('createTodoItem: title = ', title);
-        const todoItem = {
-            id: crypto.randomUUID(),
-            title,
-            completed: false
-        };
-        todoItems.push(todoItem);
-        console.debug('createTodoItem: todoItem = ', todoItem);
+        let newItem = await DatasyncService.createTodoItem(title);
+        todoItems.push(newItem);
     }
 
-    const editTodoItem = (todoItem, changes) => {
+    const editTodoItem = async (todoItem, changes) => {
         console.debug('editTodoItem: todoItem = ', todoItem, 'changes = ', changes);
-        const todoItemToEdit = todoItems.find(item => item.id === todoItem.id);
-        if (todoItem) {
-            Object.assign(todoItemToEdit, changes);
-            console.debug('editTodoItem: todoItemToEdit = ', todoItemToEdit);
+        const idx = todoItems.findIndex(item => item.id == todoItem.id);
+        console.debug('editTodoItem: idx = ', idx);
+        if (idx >= 0) {
+            const todoItemWithEdits = Object.assign({}, todoItems[idx], changes);
+            todoItems[idx] = await DatasyncService.editTodoItem(todoItemWithEdits);
         }
     };
 
-    const removeTodoItem = (todoItem) => {
+    const removeTodoItem = async (todoItem) => {
+        await DatasyncService.removeTodoItem(todoItem);
         todoItems = todoItems.filter(item => item.id !== todoItem.id);
         console.debug('removeTodoItem: ', todoItem);
     };
 
-    $('#new-todo').on('keyup', (e) => {
+    $('#new-todo').on('keyup', async (e) => {
         const $input = $(e.target), val = $input.val().trim();
         if (e.which !== 13 || !val) return;
 
-        createTodoItem(val);
-
+        await createTodoItem(val);
         $input.val('');
         renderTodoItems();
     });
 
-    $('#toggle-all').on('change', (e) => {
+    $('#toggle-all').on('change', async (e) => {
         const isChecked = $(e.target).prop('checked');
         for (const todoItem of todoItems) {
             if (todoItem.completed !== isChecked) {
-                editTodoItem(todoItem, { completed: isChecked });
+                await editTodoItem(todoItem, { completed: isChecked });
             }
         }
         renderTodoItems();
     });
 
-    $('#footer').on('click', '.clear-completed', (e) => {
+    $('#footer').on('click', '.clear-completed', async (e) => {
         for (const todoItem of todoItems) {
             if (todoItem.completed) {
-                removeTodoItem(todoItem);
+                await removeTodoItem(todoItem);
             }
         }
         listFilter = 'all';
         renderTodoItems();
     });
 
-    $('#todo-list').on('change', '.toggle', (e) => {
+    $('#todo-list').on('change', '.toggle', async (e) => {
         const idx = indexFromEl(e.target);
-        editTodoItem(todoItems[idx], { completed: !todoItems[idx].completed });
+        await editTodoItem(todoItems[idx], { completed: !todoItems[idx].completed });
         renderTodoItems();
     });
 
@@ -127,11 +182,11 @@
         }
     });
 
-    $('#todo-list').on('focusout', '.edit', (e) => {
+    $('#todo-list').on('focusout', '.edit', async (e) => {
         const el = e.target, $el = $(el), val = $el.val().trim();
 
         if (!val) {
-            removeTodoItem(todoItems[indexFromEl(el)]);
+            await removeTodoItem(todoItems[indexFromEl(el)]);
             renderTodoItems();
             return;
         }
@@ -139,14 +194,14 @@
         if ($el.data('abort')) {
             $el.data('abort', false);
         } else {
-            editTodoItem(todoItems[indexFromEl(el)], { title: val });
+            await editTodoItem(todoItems[indexFromEl(el)], { title: val });
         }
 
         renderTodoItems();
     });
 
-    $('#todo-list').on('click', '.destroy', (e) => {
-        removeTodoItem(todoItems[indexFromEl(el)]);
+    $('#todo-list').on('click', '.destroy', async (e) => {
+        await removeTodoItem(todoItems[indexFromEl(el)]);
         renderTodoItems();
     });
 
@@ -156,4 +211,12 @@
             renderTodoItems();
         }
     }).init('/all');
+
+    // Fetch the items from the service.
+    DatasyncService.fetchTodoItems().then(items => {
+        todoItems = items;
+        renderTodoItems();
+    }).catch(err => {
+        console.error(err);
+    });
 });
