94f9224900da00b6b5b7f704aad6f8441caad5a9
Adrian Hall <adhal@microsoft.com>
2024-11-22 12:06:06 -0800

    (datasync-day5) final

---
+++ datasync-day5/TodoApp.Service/Controllers/ServiceHub.cs
@@ -0,0 +1,14 @@
+﻿using CommunityToolkit.Datasync.Server;
+using Microsoft.AspNetCore.SignalR;
+using System.Text.Json;
+
+namespace TodoApp.Service.Controllers;
+
+public class ServiceHub(IDatasyncServiceOptions options) : Hub
+{
+    public async Task SendMessage(RepositoryUpdatedEventArgs eventArgs)
+    {
+        string jsonContent = JsonSerializer.Serialize(eventArgs, options.JsonSerializerOptions);
+        await Clients.All.SendAsync("ServiceChange", jsonContent);
+    }
+}
--- datasync-day5/TodoApp.Service/Controllers/TodoItemsController.cs
+++ datasync-day5/TodoApp.Service/Controllers/TodoItemsController.cs
@@ -1,6 +1,7 @@
 ﻿using CommunityToolkit.Datasync.Server;
 using CommunityToolkit.Datasync.Server.EntityFrameworkCore;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.SignalR;
 using TodoApp.Service.Database;
 
 namespace TodoApp.Service.Controllers;
@@ -8,8 +9,24 @@ namespace TodoApp.Service.Controllers;
 [Route("tables/todoitems")]
 public class TodoItemsController : TableController<TodoItem>
 {
-    public TodoItemsController(TodoContext context) : base()
+    public TodoItemsController(
+        TodoContext context,
+        IHubContext<ServiceHub> hubContext
+        ) : base()
     {
         Repository = new EntityTableRepository<TodoItem>(context);
+        RepositoryUpdated += async (sender, eventArgs) => await hubContext.Clients.All.SendAsync("ServiceChange", eventArgs);
+    }
+}
+
+
+public class EventingProvider<T>(
+    IHubContext<ServiceHub> hubContext
+    ) : AccessControlProvider<T>() where T : EntityTableData
+{
+    public override async ValueTask PostCommitHookAsync(TableOperation operation, T entity, CancellationToken cancellationToken = default)
+    {
+        RepositoryUpdatedEventArgs eventArgs = new(operation, typeof(T).Name, entity);
+        await hubContext.Clients.All.SendAsync("ServiceChange", eventArgs, cancellationToken);
     }
 }
