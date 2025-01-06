c6141679ec36e3183ee0343e471b5d6c20f83d6d
Adrian Hall <adhal@microsoft.com>
2025-01-06 13:49:35 -0800

    datasync-day6

---
+++ datasync-day6/ClientApp/Services/OnlineTodoService.cs
@@ -0,0 +1,57 @@
+﻿using ClientApp.Interfaces;
+using ClientApp.Models;
+using CommunityToolkit.Datasync.Client;
+using CommunityToolkit.Datasync.Client.Http;
+
+namespace ClientApp.Services;
+
+public class OnlineTodoService : ITodoService
+{
+    private const string baseUrl = "https://localhost:7181";
+    private readonly DatasyncServiceClient<TodoItem> client;
+
+    public OnlineTodoService()
+    {
+        var clientOptions = new HttpClientOptions()
+        {
+            Endpoint = new Uri(baseUrl)
+        };
+        client = new(clientOptions);
+    }
+
+    public async Task<TodoItem> AddTodoItemAsync(string title, CancellationToken cancellationToken = default)
+    {
+        ServiceResponse<TodoItem> response = await client.AddAsync(new TodoItem { Title = title }, cancellationToken);
+        if (response.IsSuccessful && response.HasValue)
+        {
+            return response.Value!;
+        }
+        throw new ApplicationException(response.ReasonPhrase);
+    }
+
+    public async Task<List<TodoItem>> GetAllTodoItemsAsync(CancellationToken cancellationToken = default)
+    {
+        var list = await client.ToListAsync(cancellationToken);
+        return list;
+    }
+
+    public async Task<TodoItem?> GetTodoItemAsync(string id, CancellationToken cancellationToken = default)
+    {
+        ServiceResponse<TodoItem> response = await client.GetAsync(id, cancellationToken);
+        if (response.IsSuccessful && response.HasValue)
+        {
+            return response.Value!;
+        }
+        throw new ApplicationException(response.ReasonPhrase);
+    }
+
+    public async Task<TodoItem> ReplaceTodoItemAsync(TodoItem updatedItem, CancellationToken cancellationToken = default)
+    {
+        ServiceResponse<TodoItem> response = await client.ReplaceAsync(updatedItem, cancellationToken);
+        if (response.IsSuccessful && response.HasValue)
+        {
+            return response.Value!;
+        }
+        throw new ApplicationException(response.ReasonPhrase);
+    }
+}
---
+++ datasync-day6/ServerApp/Controllers/TodoItemController.cs
@@ -0,0 +1,10 @@
+﻿using CommunityToolkit.Datasync.Server;
+using Microsoft.AspNetCore.Mvc;
+using ServerApp.Models;
+
+namespace ServerApp.Controllers;
+
+[Route("tables/todoitem")]
+public class TodoItemController(IRepository<TodoItem> repository) : TableController<TodoItem>(repository)
+{
+}
