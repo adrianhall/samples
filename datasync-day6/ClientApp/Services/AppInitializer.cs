8bccfbcb0c9a783ea28e9a80f4899c6a8a98dfbb
Adrian Hall <adhal@microsoft.com>
2025-01-06 11:51:18 -0800

    Added base ClientApp.

---
+++ datasync-day6/ClientApp/Services/AppInitializer.cs
@@ -0,0 +1,11 @@
+﻿using ClientApp.Interfaces;
+
+namespace ClientApp.Services;
+
+public class AppInitializer : IAppInitializer
+{
+    public Task Initialize()
+    {
+        return Task.CompletedTask;
+    }
+}
